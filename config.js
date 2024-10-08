fcbee36210410ee29f0ec531fcbd83b82e4a4716
KavishkaPramod <pramodkavishka133@gmail.com>
2024-10-08 20:45:18 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=LyI0QJTL#KXcqe0vwHyVpICCsC3ropLiqgSZk0_R5EcqhJMHcGVI",
 MONGODB: process.env.MONGODB || "",//enter mongo db url
 };
